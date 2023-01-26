923dc5b81d77a911a2a68b5f9052cb7c7bfe7a59
44Chick <to44guchi@gmail.com>
2023-01-26 09:16:43 +0900

    feat : socket test
    소켓 통신 테스트 완료
    (권함 없음. 추후 작업 필요)

--- src/pages/chat/ChatDetail.jsx
+++ src/pages/chat/ChatDetail.jsx
@@ -1,33 +1,100 @@
 import React from "react";
+import { useState } from "react";
 import { useEffect } from "react";
 import SockJS from "sockjs-client";
-// import Stomp from "stompjs";
+import Stomp from "stompjs";
 import { instance } from "../../api/axiosApi";
 
+const baseUrl = process.env.REACT_APP_SERVER;
+
 function ChatDetail() {
-  const sock = new SockJS("/ws/chat");
-  // const ws = Stomp.over(sock);
-  // const reconnect = 0;
+  const sock = new SockJS(`${baseUrl}/ws/chat`);
+  const ws = Stomp.over(sock);
+  const reconnect = 0;
 
   const roomId = localStorage.getItem('wschat.roomId');
   const sender = localStorage.getItem('wschat.sender');
 
-  const findRoom = () =>{
-    try{
-      const data = instance.get('/chat/room/'+ roomId);
-      console.log(data)
-    }catch(error){
-      console.log(error)
-    }
+  const [roomInfo, setRoomInfo] = useState();
+  const [msg, setMsg] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  const findRoom = () => {
+    instance.get('/chat/room/' + roomId).then(response => { setRoomInfo(response.data); });
   }
-  useEffect(()=>{
+  const sendMsg = () => {
+    ws.send("/app/chat/message", {}, JSON.stringify({ type: 'TALK', roomId: roomId, sender: sender, message: msg }));
+    setMsg("")
+  }
+  // const recvMsg = (recv) => {
+  //   console.log(messages)
+  //   const newMsg = [...messages , { "type": recv.type, "sender": recv.type == 'ENTER' ? '[알림]' : recv.sender, "message": recv.message }]
+  //   setMessages(newMsg);
+  // }
+
+  // sendMessage: function() {
+  //   ws.send("/app/chat/message", {}, JSON.stringify({type:'TALK', roomId:this.roomId, sender:this.sender, message:this.message}));
+  //   this.message = '';
+  // },
+  // recvMessage: function(recv) {
+  //     this.messages.unshift({"type":recv.type,"sender":recv.type=='ENTER'?'[알림]':recv.sender,"message":recv.message})
+  // }
+
+  const connect = () => {
+    // pub/sub event
+    ws.connect({}, function (frame) {
+      ws.subscribe("/topic/chat/room/" + roomId, function (message) {
+        const recv = JSON.parse(message.body);
+        console.log("recover",recv);
+        // setMessages([recv, ...messages])
+        messages.unshift(recv)
+        setMessages(messages)
+        // recvMsg(recv);
+      });
+      ws.send("/app/chat/message", {}, JSON.stringify({ type: 'ENTER', roomId: roomId, sender: sender }));
+    }, function (error) {
+      if (reconnect++ <= 5) {
+        console.log(error)
+        setTimeout(function () {
+          console.log("connection reconnect");
+          sock = new SockJS(`${baseUrl}/ws/chat`);
+          ws = Stomp.over(sock);
+          connect();
+        }, 10 * 1000);
+      }
+    });
+  }
+
+  useEffect(() => {
     findRoom();
-  },[])
+    connect();
+  }, [])
 
 
-  return(
+  return (
     <div>
+      <h1>{roomInfo?.roomName}</h1>
       Chat Detail
+      <div>
+        <input
+          value={msg}
+          onChange={(event) => { setMsg(event.target.value) }}
+        />
+        <button onClick={sendMsg}>전송</button>
+      </div>
+      {(messages !== []) && (
+        messages?.map((v,i) => {
+          return (
+            <div key={i}>
+              <div>User: {v.sender}</div>
+              <div>Comments: {v.message}</div>
+              <br />
+              <br />
+            </div>
+          )
+        })
+      )
+      }
     </div>
   )
 }
